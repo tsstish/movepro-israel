6ffdae1d8b71ec21e02d61d823f851e821f1e08d
tsstish <tss.tish.tss@gmail.com>
2026-07-26 12:04:21 +0300

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl = "https://movepro-israel.vercel.app";
+  const siteUrl = "https://moveproisrael.online";
 
   return {
     rules: {
