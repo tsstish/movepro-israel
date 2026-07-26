33fc91206ae296c20214077c23cb7f86b3813962
tsstish <tss.tish.tss@gmail.com>
2026-07-26 12:31:50 +0300

    Add OrganizationSchema component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import OrganizationSchema from "@/components/OrganizationSchema";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -97,6 +98,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-full antialiased`}
       >
+        <OrganizationSchema />
         {children}
       </body>
     </html>
